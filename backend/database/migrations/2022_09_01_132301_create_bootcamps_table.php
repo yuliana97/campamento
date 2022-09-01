8dec1f631b4d321da069784c2185fc34c2bed58b
Exfrom <dscelis@misena.edu.co>
2022-09-01 09:14:54 -0500

    migraciones

---
+++ backend/database/migrations/2022_09_01_132301_create_bootcamps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bootcamps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->string('website', 150);
+            $table->string('phone', 30);
+            $table->float('average_rating', 1, 1);
+            $table->float('average_cost', 8, 2);
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bootcamps');
+    }
+};
---
+++ backend/database/migrations/2022_09_01_132457_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 100);
+            $table->integer('weeks');
+            $table->float('average_cost', 8, 2);
+            $table->string('minimum_skill', 100);
+            $table->timestamps();
+            $table->foreignId('bootcamp_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
